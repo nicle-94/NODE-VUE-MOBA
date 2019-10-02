7a62d4a2aa95df19b4f79c822e588ecfc8716eff
wuxiaoying <1121900174qq.com>
2019-10-03 00:49:56 +0800

    categoryEdit

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const app = express()
+
+app.use(require('cors')())//跨域
+app.use(express.json())//就能使用req.body
+
+require('./plugins/db')(app)
+require('./routes/admin')(app)
+
+app.listen(3000, () => {
+    console.log('http://localhost:3000')
+})
